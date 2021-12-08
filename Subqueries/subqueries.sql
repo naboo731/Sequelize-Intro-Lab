ceb3029761debbdabf8cfd078690f0d4149bcb8e
Nichole Boothman <nbooth731@gmail.com>
2021-12-08 18:18:17 -0500

    Complete

--- Subqueries/subqueries.sql
+++ Subqueries/subqueries.sql
@@ -4,12 +4,41 @@ FROM invoice
 WHERE invoice_id IN (
   SELECT invoice_id FROM invoice_line
   WHERE unit_price > 0.99);
+
 -- Get all playlist tracks where the playlist name is Music.
+SELECT * 
+FROM playlist_track
+WHERE playlist_id IN (
+  SELECT playlist_id FROM playlist
+  WHERE name = 'Music');
 
 -- Get all track names for playlist_id 5.
+SELECT * 
+FROM track
+WHERE name IN (
+  SELECT name FROM playlist
+  WHERE playlist_id = 5);
 
 -- Get all tracks where the genre is Comedy.
+SELECT * 
+FROM track
+WHERE genre_id IN (
+  SELECT genre_id FROM genre
+  WHERE name = 'Comedy');
 
 -- Get all tracks where the album is Fireball.
+SELECT * 
+FROM track
+WHERE album_id IN (
+  SELECT album_id FROM album
+  WHERE title = 'Fireball');
 
 -- Get all tracks for the artist Queen ( 2 nested subqueries ).
+SELECT * 
+FROM track
+WHERE album_id IN (
+  SELECT album_id FROM album
+  WHERE artist_id IN (
+    SELECT artist_id FROM artist
+    WHERE name = 'Queen'));
+  
